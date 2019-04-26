e06ce3bf997cc493709eb9c1bc52d7ca827cebda
Tsundere-yp <884193931@qq.com>
2019-04-26 17:27:17 +0800

    config

--- cook--me/vue.config.js
+++ cook--me/vue.config.js
@@ -1,7 +1,16 @@
 module.exports={
     devServer:{
-        port:8088,
-        host:"127.0.0.1",
-        open:true
+        // port:8088,
+        // host:"127.0.0.1",
+        open:true,
+        proxy:{
+            "/cookme":{
+                target:"http://39.106.68.255:8080",
+                changeOrigin:true,
+                pathRewrite:{
+                    "^/cookme":"/"
+                }
+            }
+        }
     }
 }
