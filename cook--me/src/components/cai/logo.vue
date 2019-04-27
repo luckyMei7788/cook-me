9c0d70fc0031791cd4a629f519d076a907d8b259
luckyMei7788 <1600177077@qq.com>
2019-04-27 08:36:07 +0800

    小莹+完善

--- cook--me/src/components/cai/logo.vue
+++ cook--me/src/components/cai/logo.vue
@@ -1,25 +1,33 @@
 <template>
-	<div class="logo-a1">
-		<div class="logoA">
-		</div>
-		<div class="logo-nav">
-			<ul class="logo-ul">
-				<li><a href="#">欢迎光临CookMe菜场</a></li>
-				<li><a href="#">我的订单</a></li>
-				<li><a href="#">关于我们</a></li>
-				<li><a href="#">帮助中心</a></li>
+	<div class="box">
+		<div class="banner">
+			<img class="big" src="../../static/cai-img/banner01.jpg">
+			<ul class="nav">
+			
+					<li><a href="javascript:;">欢迎光临CookMe菜场</a></li>
+					
+				<router-link to="/order">
+					<li><a href="javascript:;">我的订单</a></li>
+				</router-link>
+				
+				<router-link to="/about">
+					<li><a class="a1" href="javascript:;">关于我们</a></li>
+				</router-link>
+				
+				<router-link to="/introduce">
+					<li><a href="javascript:;">帮助中心</a></li>
+				</router-link>	
 			</ul>
+				
+			<input class="one" type="search" />
+			<img class="small" src="../../static/cai-img/fdj.jpg">
 		</div>
-		<div class="logo-b">
-			<!-- <img src="../../static/shopping/img/菜市场banner01-1.jpg" /> -->
-			<div class="logo-b-input">
-				<input type="text"  /><div class="input-div"><a href="#"><img src="../../static/shopping/img/放大镜.png" /></a></div>
-			</div>
-		</div>
+		<router-view ></router-view>
 	</div>
 </template>
 
 <script>
+	// import order from "./order/order.vue"
 	export default {
 		name : "logo",
 		data() {
@@ -27,58 +35,66 @@
 				
 			};
 		}
+		
 	}
 </script>
 
 <style scoped lang="less">
-
-	*{
-		margin:0;
-		padding:0;
-	}
-	li{
-		list-style:none;
-	}
-	a{
-		text-decoration:none;
-	}
-	body{
-		width:100%;
-		height:700px;
-	}
+	
 	/* logo */
-	.logo-a{
+	.box{
 		width:1100px;
-		height:72px;
+		height:514px;
 		margin:0 auto;
-		overflow: hidden;
-	}
-	.logoA{
-		width:1100px;
-		height:72px;
-		background:url(../../static/shopping/img/菜市场banner01-1.jpg) no-repeat ;
-		overflow:hidden;
-		opacity:0.5;
-	}
-	.logo-nav{
-		width:1100px;
-		position:relative;
-		top:-50px;
-		left:50px;
-		z-index:1;
-		.logo-ul li{
-			float:left;
-			margin-left:66px;
-			.logo-nav .logo-ul li a:nth-child(1) {
-				position: relative;
-				left: -74px;
+		margin-bottom: 31px;
+		background: #fff;
+		.banner{
+			width:1100px;
+			height:514px;
+			margin:0 auto;
+			// position:relative;
+			.big{
+				width:100%;
+				height:100%;
+				//margin:0 auto;
 			}
-			a{
-				font-size:20px;
-				color:#1e7902;
-				margin-left:27px;
+			.nav{
+				width:1100px;
+				height:72px;
+				background:rgba(255,255,255,0.5);
+				position:absolute;
+				top:86px;
+				li{
+					float:left;
+					margin-left:66px;
+					line-height: 72px;
+				a{
+					font-size:20px;
+					color:#1e7902;
+				}
+				}
+			}
+			.small{
+				width:92px;
+				height:68px;
+				border:1px solid green;
+				position:absolute;
+				top:508px;
+				left:796px;
+				background:rgba(255,255,255,0.5);
+				
+			}
+			.one{
+				width:409px;
+				height:68px;
+				border:1px solid green;
+				position:absolute;
+				 top:508px;
+				 left:388px;
+				background:rgba(255,255,255,0.5);
 			}
 		}
 	}
+
 
 </style>
